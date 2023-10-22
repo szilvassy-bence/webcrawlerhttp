640cdd0dd1e424e96beee4d26eca41db2e4a65cb
Szilvássy Bence <benceszilvassy91@gmail.com>
2023-10-22 20:15:07 +0200

    getURLSFromHTML, test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0,1) === '/') {
+      //relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        console.log(urlObj);
+        urls.push(urlObj.href);
+      } catch(error) {
+        console.log(`Error with relative url: ${error.message}`)
+      }
+    } else {
+      //absolute
+      try {
+        const urlObj = new URL(`${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch(error) {
+        console.log(`Error with absolute url: ${error.message}`)
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +37,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
