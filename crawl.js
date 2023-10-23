87e6c49e2ae9a96a75b77d5b12f84a15cbe66a1e
Szilvássy Bence <benceszilvassy91@gmail.com>
2023-10-23 05:37:09 +0200

    crawler

--- crawl.js
+++ crawl.js
@@ -1,22 +1,45 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL)
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++
+    return pages
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`actively crawling: ${currentURL}`);
+
   try {
     const resp = await fetch(currentURL);
     if (resp.status > 399) {
       console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-      return;
+      return pages;
     }
     const contentType = resp.headers.get('Content-type')
     if (!contentType.includes('text/html')) {
       console.log(`no html response: ${contentType} on page: ${currentURL}`);
-      return;
+      return pages;
     }
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
   } catch(err) {
     console.log(`error in fetch: ${err.message},on page: ${currentURL}`)
   }
+  return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -28,7 +51,6 @@ function getURLsFromHTML(htmlBody, baseURL) {
       //relative
       try {
         const urlObj = new URL(`${baseURL}${linkElement.href}`);
-        console.log(urlObj);
         urls.push(urlObj.href);
       } catch(error) {
         console.log(`Error with relative url: ${error.message}`)
